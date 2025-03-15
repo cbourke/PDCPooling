1774ce54ba1b94b9f7bd8c3f4b54cd5e16be44a3
Chris Bourke <cbourke@cse.unl.edu>
2025-03-15 17:08:12 -0500

    database info class

---
+++ src/main/java/pdc/database/DatabaseInfo.java
@@ -0,0 +1,9 @@
+package pdc.database;
+
+public class DatabaseInfo {
+
+	private static final String url      = "";
+	private static final String username = "";
+	private static final String password = "";
+
+}
